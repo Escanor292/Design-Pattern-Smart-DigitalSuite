9e34840ac6ceb644b0b2a7cbfc63197a5e65898e
LHUser <LHUser@P0131>
2025-04-24 10:26:27 +0700

    bla

---
+++ DigitalOrderSystem.Models/DigitalOrderSystem.Models/OrderHandlerFacade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalOrderSystem.Models
+{
+    public class OrderHandlerFacade
+    {
+        public void Handle(Order order)
+        {
+            if (order.OrderMode == "Legacy")
+                new OrderProcessor_Old().Process(order);
+            else
+                new OrderProcessor_New().Process(order);
+        }
+    }
+}
---
+++ DigitalOrderSystem.Models/DigitalOrderSystem.Models/OrderProcessor_Old.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DigitalOrderSystem.Models;
+
+namespace DigitalOrderSystem.Models
+{
+    public class OrderProcessor_Old
+    {
+        public void Process(Order order)
+        {
+            Console.WriteLine("⚠️ Processing order using OLD system...");
+        }
+    }
+}
